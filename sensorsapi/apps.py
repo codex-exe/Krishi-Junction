a1e9677d1a17f48f27aca69a34ca484ed3e22169
shivam-akhouri <72240154+shivam-akhouri@users.noreply.github.com>
2023-05-12 23:07:43 +0530

    Delete sensorsapi directory

--- sensorsapi/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SensorsapiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sensorsapi'
--- sensorsapi/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    # path('npkvalue/', views.sensordata, name='index'),
-]
